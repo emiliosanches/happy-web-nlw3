25279f6a858120e89f74b41bc7952af88778e223
Shadow584 <55329280+Shadow584@users.noreply.github.com>
2020-10-13 00:51:51 -0300

    Added map page

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import { LandingPage } from './pages/Landing';
+import { OrphanagesMap } from './pages/OrphanagesMap';
 
 export const Router = () => (
     <BrowserRouter>
         <Switch>    
-            <Route path="/"component={LandingPage}/>
+            <Route path="/" exact component={LandingPage}/>
+            <Route path="/app" component={OrphanagesMap} />
         </Switch>
     </BrowserRouter>
 )

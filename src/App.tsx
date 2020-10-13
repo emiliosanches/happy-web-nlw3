4b8aadeac26b4f962ae5440bd1c8ddbb11abcc5f
Shadow584 <55329280+Shadow584@users.noreply.github.com>
2020-10-12 23:28:39 -0300

    Revert "Added images folder and images used in landing page"
    This reverts commit 375bd72ed6f7b5168cbff31b7ae806ee5e24b575.

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,11 @@ import React from 'react';
 import './App.css';
 
 function App() {
-    return (
-        <div>
-            
-        </div>
-    );
+  return (
+    <div>
+      
+    </div>
+  );
 }
 
 export default App;

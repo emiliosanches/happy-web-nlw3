f83836db1072219589fd3b95a9c77dbab39ff897
Shadow584 <55329280+Shadow584@users.noreply.github.com>
2020-10-14 12:58:01 -0300

    Adding Orphanage and CreateOrphanage pages

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import CreateOrphanage from './pages/CreateOrphanage';
 import { LandingPage } from './pages/Landing';
+import Orphanage from './pages/Orphanage';
 import { OrphanagesMap } from './pages/OrphanagesMap';
 
 export const Router = () => (
@@ -8,6 +10,8 @@ export const Router = () => (
         <Switch>    
             <Route path="/" exact component={LandingPage}/>
             <Route path="/app" component={OrphanagesMap} />
+            <Route path="/app" component={Orphanage} />
+            <Route path="/app" component={CreateOrphanage} />
         </Switch>
     </BrowserRouter>
 )
